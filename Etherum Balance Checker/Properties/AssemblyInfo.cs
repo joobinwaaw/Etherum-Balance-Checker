486de83dd8b36c861f5bb714a32ccb32c722796d
joobinwaw <163397161+joobinwaw@users.noreply.github.com>
2024-03-14 12:59:21 +0400

    AssemblyInfo.cs

---
+++ Etherum Balance Checker/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Etherum Balance Checker")]
+[assembly: AssemblyDescription("by unknown")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Etherum Balance Checker")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("04d9e2d6-59f7-4451-803c-c028b0178e24")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
